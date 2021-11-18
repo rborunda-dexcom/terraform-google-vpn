31475b180a03dd6342967079124b0d9a9feb1a92
Ramon S. Borunda <ramon.borunda2@dexcom.com>
2021-11-18 11:12:51 -0800

    corrected the data reference for the first two remote tunnels.

--- examples/us_hub/hub.tf
+++ examples/us_hub/hub.tf
@@ -31,7 +31,7 @@ tunnels = {
       }
       bgp_peer_options                = null
       bgp_session_range               = "169.254.1.6/30"
-      peer_gcp_gateway                = data.google_compute_ha_vpn_gateway.eu-spoke3.name
+      peer_gcp_gateway                = data.google_compute_ha_vpn_gateway.us-spoke2.name
       ike_version                     = 2
       vpn_gateway_interface           = 0
       peer_external_gateway_interface = null
@@ -44,7 +44,7 @@ tunnels = {
       }
       bgp_peer_options                = null
       bgp_session_range               = "169.254.2.6/30"
-      peer_gcp_gateway                =  data.google_compute_ha_vpn_gateway.eu-spoke3.name
+      peer_gcp_gateway                =  data.google_compute_ha_vpn_gateway.us-spoke2.name
       ike_version                     = 2
       vpn_gateway_interface           = 1
       peer_external_gateway_interface = null
