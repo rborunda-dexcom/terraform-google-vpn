9697ff5da54cf0f2acf6bbd0ec4b7c15fa2dda70
Ramon S. Borunda <ramon.borunda2@dexcom.com>
2021-11-18 13:36:35 -0800

    asdf

--- examples/us_hub/hub.tf
+++ examples/us_hub/hub.tf
@@ -89,19 +89,7 @@ data "google_compute_ha_vpn_gateway" "vpn-hub" {
 
 }
 
-data "google_compute_ha_vpn_gateway" "data-spoke1-to-hub" {
-  name             = "spoke1-to-hub"
-  project = "us-spoke1"
-  region = "us-central1"
 
-}
-
-data "google_compute_ha_vpn_gateway" "us-spoke2" {
-  name             = "us-spoke2"
-  project = "ringed-furnace-331520"
-  region = "us-central1"
-
-}
 
 data "google_compute_router" "vpn-hub" {
   name  = "vpn-${var.name}"
